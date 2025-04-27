3c2b5cfff8e160f41767fc9187f43cecab14e540
juliannatruitt <truittj3@nku.edu>
2025-04-27 19:03:47 -0400

    code updates

--- main.py
+++ main.py
@@ -8,113 +8,106 @@
 #export INFLUXDB_TOKEN=CQxPVcrv6nB7b_W5_-SIJcr4kCOd02w7Z-qxiMQZ1O8GyEDtyIu1QZwT4BkU4UXkkcuO4KMXyUBTSWShkHdIqw==
 
 #import required modules
-from xmlrpc import client
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-from gpiozero import LED
+#from gpiozero import LED
 
-#DO NOT MODIFY
-token = "CQxPVcrv6nB7b_W5_-SIJcr4kCOd02w7Z-qxiMQZ1O8GyEDtyIu1QZwT4BkU4UXkkcuO4KMXyUBTSWShkHdIqw=="
-org = "NKU"
-#Uncomment the desired IP.
-#url = "http://10.15.8.77:8086"  #Pi at NKU
-url = "http://172.16.1.100:8086" #Pi at Nick's VPN
+# Constants (DO NOT MODIFY)
+TOKEN = "CQxPVcrv6nB7b_W5_-SIJcr4kCOd02w7Z-qxiMQZ1O8GyEDtyIu1QZwT4BkU4UXkkcuO4KMXyUBTSWShkHdIqw=="
+ORG = "NKU"
+URL = "http://172.16.1.100:8086"  # Pi at Nick's VPN
+BUCKET = "group1"
 
-write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-bucket="group1"
-write_api = write_client.write_api(write_options=SYNCHRONOUS)
-query_api = write_client.query_api()
+# Setup InfluxDB client
+client = InfluxDBClient(url=URL, token=TOKEN, org=ORG)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+query_api = client.query_api()
 
-# relay for internet control
-relay = LED(17)  # Replace with whatever GPIO pin
+# Setup GPIO relay for internet control
+# relay = LED(17)
+relay = "OFF"
 
-# global variables to track state
+# State tracking variables
 internet_is_on = False
-internet_end_time = 0
 
+# Secure priority registry
+device_priorities = {
+    "device1": "high",
+    "device2": "low",
+    "device3": "low",
+    "device4": "high",
+}
 
-# function to check for internet requests
+# Function to check recent internet requests
 def check_for_requests():
-    # query for the most recent internet requests in the last 1 minutes
-    query = f"""from(bucket: "{bucket}")
+    query = f'''
+    from(bucket: "{BUCKET}")
       |> range(start: -1m)
       |> filter(fn: (r) => r._measurement == "internet_request")
       |> filter(fn: (r) => r._field == "requesting")
-      |> filter(fn: (r) => r.value == true)
-      |> last()""" # use last to only get the most recent request if many are sent. this is probably best practice so that we do not need to go through many requests if multiple devices request (since we will turn on internet either way)
+      |> filter(fn: (r) => r._value == true)
+    '''
 
-    tables = query_api.query(query, org=org)
+    tables = query_api.query(query, org=ORG)
 
-    # process any requests found
-    requests_found = False
+    requests_found = []
     for table in tables:
         for record in table.records:
-            requests_found = True
             device_id = record.values.get("device_id", "unknown")
-            print(f"Found internet request from device: {device_id}")
+            priority = device_priorities.get(device_id, "low")
+            requests_found.append((device_id, priority))
+            print(f"Request from device: {device_id}, Priority: {priority}")
 
     return requests_found
 
+# Function to activate internet access
 def turn_on_internet():
-    global internet_is_on, internet_end_time
+    global internet_is_on, relay
 
-    # update the state variables
     internet_is_on = True
-    internet_end_time = time.time() + (5 * 60) # will keep internet on for 5 minutes (or 300 seconds)
+    relay = "ON"
 
-    # turn on the relay, which the internet is plugged into
-    relay.on()
+    print(f"internet is ", relay)
 
-    # should we have an internet status point too in our bucket??
+    # Notify high priority devices immediately
+    point_high = Point("internet_access").tag("priority", "high").field("available", True)
+    write_api.write(bucket=BUCKET, org=ORG, record=point_high)
+    print("High priority devices notified of internet ON")
 
-    point = Point("internet_access").tag("priority", "high").field("available", True).field("duration_in_minutes", 5)
-    write_api.write(bucket=bucket, org=org, record=point)
-    print("High priority devices notfied that internet is on for next 5 minutes")
-
-    # delay of 10 seconds for low priority devices to be notified
     time.sleep(10)
 
-    point = Point("internet_access").tag("priority", "low").field("available", True).field("duration_in_minutes", 5)
-    write_api.write(bucket=bucket, org=org, record=point)
-    print("Low priority devices notfied that internet is on for next 5 minutes")
+    # Notify low priority devices
+    point_low = Point("internet_access").tag("priority", "low").field("available", True)
+    write_api.write(bucket=BUCKET, org=ORG, record=point_low)
+    print("Low priority devices notified of internet ON")
 
+# Function to deactivate internet access
 def turn_off_internet():
-    global internet_is_on, internet_end_time
+    global internet_is_on, relay
 
-    # update the state variables
     internet_is_on = False
-    internet_end_time = 0
+    relay = "OFF"
 
-    #turn relay off
-    relay.off()
+    print(f"internet is ", relay)
 
-    point = Point("internet_access").tag("priority", "high").field("available", False).field("duration_in_minutes", 0)
-    write_api.write(bucket=bucket, org=org, record=point)
+    # Update database about internet off state
+    for priority in ["high", "low"]:
+        point_off = Point("internet_access").tag("priority", priority).field("available", False)
+        write_api.write(bucket=BUCKET, org=ORG, record=point_off)
 
-    point = Point("internet_access").tag("priority", "low").field("available", False).field("duration_in_minutes", 0)
-    write_api.write(bucket=bucket, org=org, record=point)
+    print("Internet turned OFF")
 
-    print("Internet turned off")
-
-
-# function to determine when internet needs to be turned off (tracks the internet_end_time!)
-def determine_when_to_turnoff_internet():
-    global internet_is_on, internet_end_time
-
-    # since internet_end_time is set as the current time + 5 minutes, this code will detect when 5 minutes has past if the current time is >= to the internet_end_time
-    if internet_is_on and time.time() >= internet_end_time:
-        turn_off_internet()
-
-# main loop: checks if internet needs to be turned off, checks requests (if internet is not on), sleeps every second?
+# Main loop
 while True:
-    if internet_is_on:
-        determine_when_to_turnoff_internet()
-
-    if not internet_is_on:
-        check_requests = check_for_requests()
-        if check_requests:
+    requests = check_for_requests()
+    if requests:
+        if not internet_is_on:
+            print("Activating internet for received requests.")
             turn_on_internet()
+    else:
+        if internet_is_on:
+            print("No active requests. Deactivating internet.")
+            turn_off_internet()
 
-    # sleep for 1 second
     time.sleep(1)
--- test_internet_request.py
+++ test_internet_request.py
@@ -1,37 +1,52 @@
-#CIT-381 Final Project InfluxDB Client template
+# creating testing request
 
-#You will need to install the InfluxDB Client on your local machine. Python3 must be installed.
-#run the command below to install InfluxDB Client:
-#pip3 install influxdb-client
-
-#Run this command in your terminal to save your token as an environment variable:
-#export INFLUXDB_TOKEN=CQxPVcrv6nB7b_W5_-SIJcr4kCOd02w7Z-qxiMQZ1O8GyEDtyIu1QZwT4BkU4UXkkcuO4KMXyUBTSWShkHdIqw==
-
-#import required modules
-from xmlrpc import client
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 # from gpiozero import LED
 
-#DO NOT MODIFY
+# DO NOT MODIFY
 token = "CQxPVcrv6nB7b_W5_-SIJcr4kCOd02w7Z-qxiMQZ1O8GyEDtyIu1QZwT4BkU4UXkkcuO4KMXyUBTSWShkHdIqw=="
 org = "NKU"
-#Uncomment the desired IP.
-#url = "http://10.15.8.77:8086"  #Pi at NKU
-url = "http://172.16.1.100:8086" #Pi at Nick's VPN
+# Uncomment the desired IP.
+# url = "http://10.15.8.77:8086"  #Pi at NKU
+url = "http://172.16.1.100:8086"  # Pi at Nick's VPN
 
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-bucket="group1"
+bucket = "group1"
 write_api = write_client.write_api(write_options=SYNCHRONOUS)
 query_api = write_client.query_api()
 
+
 def send_internet_request():
-    point = Point("internet_request").tag("device_id", "alarm").tag("priority", "high").field("requesting", True)
+    point = Point("internet_request").tag("device_id", "device1").field("requesting", True)
     write_api.write(bucket="group1", org=org, record=point)
+
+
+def check_internet_status():
+    query = f"""from(bucket: "group1")
+    |> range(start: -1m)
+    |> filter(fn: (r) => r._measurement == "internet_access")
+    |> filter(fn: (r) => r._field == "available")
+    |> last()"""
+
+    tables = query_api.query(query, org=org)
+
+    internet_available = False
+    for table in tables:
+        for record in table.records:
+            # Access the value correctly
+            internet_available = record.get_value()
+            print(f"Internet available: {internet_available}")
+
+    return internet_available
+
 
 while True:
     print("sending request")
+    print(check_internet_status())
     send_internet_request()
-    print("request sent")
+    time.sleep(10)
+    print(check_internet_status())
     time.sleep(350)
+
