b5154f714436e55757493619ce26d46f6ccc76d8
juliannatruitt <truittj3@nku.edu>
2025-04-29 18:29:47 -0400

    code priority updates

--- main.py
+++ main.py
@@ -11,12 +11,13 @@
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-from gpiozero import LED
+# from gpiozero import LED
 
 # Constants (DO NOT MODIFY)
 token = "CQxPVcrv6nB7b_W5_-SIJcr4kCOd02w7Z-qxiMQZ1O8GyEDtyIu1QZwT4BkU4UXkkcuO4KMXyUBTSWShkHdIqw=="
 org = "NKU"
-url = "http://10.5.12.45:8086"  #Pi at NKU
+# url = "http://10.5.12.45:8086"  #Pi at NKU
+url = "http://172.16.1.100:8086" #nicks pi
 bucket = "group1"
 
 # Setup InfluxDB client
@@ -25,11 +26,16 @@ write_api = client.write_api(write_options=SYNCHRONOUS)
 query_api = client.query_api()
 
 # Setup GPIO relay for internet control
-relay = LED(6)
+relay = "OFF"
+# relay = LED(6)
 
 
 # State tracking variables
 internet_is_on = False
+internet_end_time = 0
+current_requests = []
+queue_end_timer = 0
+queue_timer_started = False
 
 # Secure priority registry
 device_priorities = {
@@ -41,6 +47,8 @@ device_priorities = {
 
 # Function to check recent internet requests
 def check_for_requests():
+    global current_requests
+
     query = f'''
     from(bucket: "{bucket}")
       |> range(start: -1m)
@@ -50,25 +58,38 @@ def check_for_requests():
     '''
 
     tables = query_api.query(query, org=org)
-
-    requests_found = []
+    print(current_requests)
     for table in tables:
         for record in table.records:
             device_id = record.values.get("device_id", "unknown")
+            # checking if the device id is in current request will avoid duplicate requests from being counted
+            if any(device_id == req[0] for req in current_requests):
+                continue
             priority = device_priorities.get(device_id, "low")
-            requests_found.append((device_id, priority))
+            current_requests.append((device_id, priority))
             print(f"Request from device: {device_id}, Priority: {priority}")
 
-    return requests_found
+def start_queue_timer():
+    global queue_end_timer, queue_timer_started
+
+    queue_timer_started = True
+    # this set the end of the queue (waiting period for a low priority request to be processed)
+    # to 10 minuted from the current time. This will give low priority requests a maximum of 10 minutes before
+    # internet is turned on to see if more requests come in
+    queue_end_timer = time.time() + 600
 
 # Function to activate internet access
 def turn_on_internet():
-    global internet_is_on, relay
+    global internet_is_on, relay, queue_timer_started, queue_end_timer, internet_end_time, current_requests
+
+    # reset variables to original state
+    queue_timer_started = False
+    queue_end_timer = 0
+    current_requests = []
 
     internet_is_on = True
-    relay.on()
+    relay="ON"
 
-    print(f"internet is ", relay)
 
     # Notify high priority devices immediately
     point_high = Point("internet_access").tag("priority", "high").field("available", True)
@@ -82,14 +103,16 @@ def turn_on_internet():
     write_api.write(bucket=bucket, org=org, record=point_low)
     print("Low priority devices notified of internet ON")
 
+    # set the internet to turn off in 2 minutes
+    internet_end_time = time.time() + 120
+
 # Function to deactivate internet access
 def turn_off_internet():
-    global internet_is_on, relay
+    global internet_is_on, relay, internet_end_time
 
     internet_is_on = False
-    relay.off()
-
-    print(f"internet is ", relay)
+    internet_end_time = 0
+    relay="OFF"
 
     # Update database about internet off state
     for priority in ["high", "low"]:
@@ -100,13 +123,44 @@ def turn_off_internet():
 
 # Main loop
 while True:
-    requests = check_for_requests()
-    if requests:
-        if not internet_is_on:
-            print("Activating internet for received requests.")
-            turn_on_internet()
+
+    if not internet_is_on:
+        check_for_requests()
+
+        if current_requests:
+
+            # we have to set these to 0 before counting requests each time or else it will keep counting the
+            # same request multiple times. when we initalize it to 0 before counting each time, we get an accurate
+            # number of the amount of requests being recieved
+            high_priority_requests = 0
+            low_priority_requests = 0
+
+            for request in current_requests:
+                if request[1] == "high":
+                    high_priority_requests = high_priority_requests + 1
+                else:
+                    low_priority_requests = low_priority_requests + 1
+            print(f"number of high requests: ", high_priority_requests)
+            print(f"number of low priority requests: ", low_priority_requests)
+
+            # if there is a high priority request, we immediatly turn on the internet
+            if high_priority_requests > 0:
+                turn_on_internet()
+                print("Activating internet for received requests.")
+                continue
+
+            # will get to this line if there is not at least one high priority request, so we will need to
+            # start the queue timer for low priority requests (maximum 10 minutes) if it is not already started
+            if not queue_timer_started:
+                start_queue_timer()
+
+            # turn on internet if there are at least 3 low priority requests or if the 10 minute waiting period is done
+            if low_priority_requests >= 3 or time.time() >= queue_end_timer:
+                turn_on_internet()
+                print("Activating internet for received requests.")
     else:
-        if internet_is_on:
+        # if the two minutes that internet has been on passes, we should turn off the internet
+        if time.time() >= internet_end_time:
             print("No active requests. Deactivating internet.")
             turn_off_internet()
 
--- test_internet_request.py
+++ test_internet_request.py
@@ -9,8 +9,8 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 token = "CQxPVcrv6nB7b_W5_-SIJcr4kCOd02w7Z-qxiMQZ1O8GyEDtyIu1QZwT4BkU4UXkkcuO4KMXyUBTSWShkHdIqw=="
 org = "NKU"
 # Uncomment the desired IP.
-url = "http://10.5.12.45:8086"  #Pi at NKU
-#url = "http://172.16.1.100:8086"  # Pi at Nick's VPN
+#url = "http://10.5.12.45:8086"  #Pi at NKU
+url = "http://172.16.1.100:8086"  # Pi at Nick's VPN
 
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 bucket = "group1"
@@ -18,22 +18,14 @@ write_api = write_client.write_api(write_options=SYNCHRONOUS)
 query_api = write_client.query_api()
 
 
-def send_internet_request_high_priority():
-    point = Point("internet_request").tag("device_id", "device1").field("requesting", True)
-    write_api.write(bucket="group1", org=org, record=point)
-
-def send_internet_request_high_low():
-    point = Point("internet_request").tag("device_id", "device2").field("requesting", True)
-    write_api.write(bucket="group1", org=org, record=point)
-
-def send_internet_request_high_unknown():
-    point = Point("internet_request").tag("device_id", "device1234").field("requesting", True)
+def send_internet_request(device):
+    point = Point("internet_request").tag("device_id", device).field("requesting", True)
     write_api.write(bucket="group1", org=org, record=point)
 
 
 def check_internet_status():
     query = f"""from(bucket: "group1")
-    |> range(start: -1m)
+    |> range(start: -2m)
     |> filter(fn: (r) => r._measurement == "internet_access")
     |> filter(fn: (r) => r._field == "available")
     |> last()"""
@@ -51,13 +43,13 @@ def check_internet_status():
 
 
 while True:
-    print("sending request")
     print(check_internet_status())
-    send_internet_request_high_priority()
-    send_internet_request_high_low()
-    time.sleep(25)
-    send_internet_request_high_unknown()
+    send_internet_request("TEST1") # unknown device_id
+    time.sleep(3)
     print(check_internet_status())
-    time.sleep(350)
+    time.sleep(30)
 
+    send_internet_request("device1") # high priority, internet should turn on
+    time.sleep(145)
 # test request from low priorty, high priority, and unknow device ID
+# show how when high priority device comes in, they automatically get internet on
